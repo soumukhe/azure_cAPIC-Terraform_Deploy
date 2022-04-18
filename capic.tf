9ee805e924821f1666cd3499aa685e5f4641db9e
soumukhe <soumukhe@cisco.com>
2022-04-18 15:29:26 -0500

    added depdends_on for arm template also

--- capic.tf
+++ capic.tf
@@ -32,6 +32,7 @@ resource "azurerm_resource_group" "rgroup" {
 
 # Deploy cAPIC from ARM template
 resource "azurerm_resource_group_template_deployment" "capic" {
+  depends_on          = [azurerm_resource_group.rgroup]
   name                = var.vmName
   resource_group_name = var.rgName
   deployment_mode     = "Incremental"
