cfeaf27f07c04814dad095759e4bb5bbdda95821
soumukhe <soumukhe@cisco.com>
2022-04-18 18:48:16 -0500

    removed unused shell file

--- remove-capic-tf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-terraform state list | while read line
-do
-echo "deleting: " $line
-terraform destroy -target $line -auto-approve
-done
-~                                                
